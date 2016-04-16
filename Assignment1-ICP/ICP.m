11fa4f66debfbb4de8c6157b5d4de0427fb63c89
kb211 <kas_bouwens@hotmail.com>
2016-04-16 14:34:28 +0200

    svd

--- Assignment1-ICP/ICP.m
+++ Assignment1-ICP/ICP.m
@@ -2,11 +2,27 @@ R = eye(3);
 t = 0;
 
 A1 = readPcd('data/0000000000.pcd');
-A2 = readPcd('data/0000000001.pcd');
+A1(:,4) = [];
 
-[k, d] = dsearchn(A1,A2)
-%for i = 1:length(A1)
-%    for j = 1:length(A2)
-%        A1(i,:) - A2(j,:)
-%    end
-%end
+A2 = readPcd('data/0000000001.pcd');
+A2(:,4) = [];
+
+centroidA1 = sum(A1)/ length(A1);
+centroidA2 = sum(A2)/length(A2);
+
+for i = 1:length(A1)
+    A1(i,:) = A1(i,:) - centroidA1;
+end
+
+for i = 1:length(A2)
+    A2(i,:) = A2(i,:) - centroidA2;
+end
+
+
+[k, d] = dsearchn(A1,A2);
+A = [0,0,0,0];
+for i = 1:length(k)
+    A = A + (A1(k(i),:))*(A2(i,:));
+end
+
+[U, S, V] = svd(A);
