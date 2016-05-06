a54c917aadc578f8164ab6d3418aff9306317162
Wolf Vos <wolf@mijnlieff.nl>
2016-05-06 18:00:59 +0200

    epipolar lines

--- Assignment2-SFM/PlotEpipolarLines.m
+++ Assignment2-SFM/PlotEpipolarLines.m
@@ -2,7 +2,7 @@ function PlotEpipolarLines(im1, im2, F, Epipole)
 figure();
 imshow(cat(2, im1, im2)) ;
 Epipole
-[X, Y] = ginput(1)
+[X, Y] = ginput(1);
 
 hold on ;
 h = line([X(1) ; Epipole(1)], [Y(1) ; Epipole(2)]) ;
